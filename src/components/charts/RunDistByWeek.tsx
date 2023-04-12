7de659e62011415122ae938fdc18c38c578f5770
Rob Monday <10903531+robmonday@users.noreply.github.com>
2023-04-11 22:06:31 -0400

    fixing import issue

--- src/components/charts/RunDistByWeek.tsx
+++ src/components/charts/RunDistByWeek.tsx
@@ -7,7 +7,38 @@ Chart.register(CategoryScale);
 import { Bar, Line } from "react-chartjs-2";
 import { ChartData } from "chart.js/auto";
 
-import { Data } from "../../Data";
+const Data = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 45677,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 78888,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 90000,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 4300,
+    userLost: 234,
+  },
+];
 
 export default function RunDistByWeek() {
   const [chartData, setChartData] = useState<ChartData<"line">>({
